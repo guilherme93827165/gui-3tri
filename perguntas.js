099dca2de859ae7050247919e603e890b164f0a5
guilherme93827165 <132589955+guilherme93827165@users.noreply.github.com>
2024-10-28 11:55:52 -0300

    thrh

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Xadrez',
+    'O que é o roque?',
+    'O roque é um movimento especial do rei com a torre'
+)
+criaCartao(
+    'Naruto',
+    'Qual é a traduçaão de rasengan?',
+    'Esfera Espiral'
+)
+criaCartao(
+    'Xadrez',
+    'Qual é a anotação algébrica da rainha?',
+    'D'
+)
+criaCartao(
+    'Naruto',
+    'Qual o nome da raposa que é selada dentro do naruto?',
+    'Kurama'
+)
